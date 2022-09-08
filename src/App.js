790543ec5872cec41bd5c7d48620069aabbf21d1
Pedro <pedrosteinmuller10105@hotmail.com>
2022-09-08 17:51:30 -0300

    iniciando req 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import * as api from './services/api';
 
@@ -8,9 +8,9 @@ api.getCategories().then((categories) => { console.log(categories); });
 function App() {
   return (
     <BrowserRouter>
-      <switch>
+      <Switch>
         <Route exact path="/" component={ Home } />
-      </switch>
+      </Switch>
     </BrowserRouter>
   );
 }
