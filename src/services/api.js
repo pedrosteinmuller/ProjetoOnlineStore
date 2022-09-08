1dfad4c9dac58044e2eab7c949c77519d65f90f1
Pedro <pedrosteinmuller10105@hotmail.com>
2022-09-08 15:48:24 -0300

    resolucao requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,23 @@
 export async function getCategories() {
-  // Implemente aqui
+  const responseApi = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const dataApi = responseApi.json();
+  console.log(dataApi);
+  return dataApi;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(...infos) {
+  const [categoryId, query] = infos;
+  if (infos.length === 2) {
+    const categoryAndQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+    return categoryAndQuery.json();
+  }
+  const idCategory = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  return idCategory.json();
 }
 
-export async function getProductById() {
+export async function getProductById(id) {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+  const getId = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  return getId.json();
 }
