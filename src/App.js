620ccfcc2bc999b402ba787bd51055213d0fa12c
Pedro <pedrosteinmuller10105@hotmail.com>
2022-09-08 18:52:12 -0300

    resolucao requisito 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import * as api from './services/api';
-
-api.getCategories().then((categories) => { console.log(categories); });
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/Cart" component={ Cart } />
       </Switch>
     </BrowserRouter>
   );
